59b2b5f28a160f2882066fc6eef9579a8586f636
diega <diegochiola@gmail.com>
2024-03-18 23:16:54 +0100

    create api.php and routes

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->get('/user', function(Request$request){
+    return $request->user();
+});
+//creamos
+Route::group(['prefix' => 'v1'], function(){
+    Route::apiResource('customers',CustomerController::class);
+    Route::apiResource('invoices',InvoiceController::class);
+});
+
